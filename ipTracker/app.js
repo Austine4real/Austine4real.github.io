cc7fe4f05908d342d52644ba34a802139be40118
Ugbodaga Augustine <83676555+Austine4real@users.noreply.github.com>
2023-06-19 00:27:00 +0100

    update

--- ipTracker/app.js
+++ ipTracker/app.js
@@ -1,194 +1,91 @@
-'use strict'
-import API_KEY from './config.js'
+const myForm = document.querySelector('form');
+const myIpAddr = document.querySelector('.ip-addr h2');
+const myLocation = document.querySelector('.location h2');
+const myTimezone = document.querySelector('.timezone h2');
+const myIsp = document.querySelector('.isp h2');
+// Map creation
+const myMap = L.map('myMap').setView([51.505, -0.09], 13);
 
-const ipAddressField = document.querySelector('.ipAddressField')
-const timezoneInput = document.querySelector('.timezoneInput')
-const countryLocationInput = document.querySelector('.locationInput')
-const ispInput = document.querySelector('.ispInput')
-const submitBtn = document.querySelector('.submit-btn')
-const inputField = document.querySelector('.input-field')
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
 
-//Map
+const myIcon = L.icon({
+  iconUrl: './images/icon-location.svg',
+  iconSize: [40, 50],
+  iconAnchor: [20, 50]
+});
 
-let map = L.map('map').setView([51.505, -0.09], 13)
+let myMarker;
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map)
+const mapDisplay = (lat, lng) => {
+  myMap.setView([lat, lng], 16);
 
-//API
-let ipAddress
-let randomIP = ''
-let timeZone
-let countryLocation
-let cityLocation
-let postalCode
-let isp
-let lat
-let lng
+  if (myMarker) myMarker.remove();
 
-let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}=`
-fetch(url)
-  .then((response) => response.json())
-  .then((response) => {
-    ipAddress = response.ip
-    timeZone = response.time_zone.offset
-    countryLocation = response.country_name
-    cityLocation = response.city
-    postalCode = response.zipcode
-    isp = response.isp
-    lat = response.latitude
-    lng = response.longitude
+  myMarker = L.marker([lat, lng], { icon: myIcon }).addTo(myMap);
+};
 
-    ipAddressField.innerHTML = ipAddress
-    timezoneInput.innerHTML = ` UTC ${timeZone}`
-    countryLocationInput.innerHTML = `${countryLocation}, ${cityLocation} ${postalCode}`
-    ispInput.innerHTML = isp
-    mapLocation(lat, lng)
-  })
+// Get the info the page needs
+const getData = (inputValue = '', searchType = 'IP') => {
+//   const apiKey = 'at_E6BD5wm4eWkq0e3q9TyyAmGNH165p'; // Replace with your actual API key
+  const apiKey = 'at_mrynU6iAELbDDua1k4TVs9OdIU59f'; // Replace with your actual API key
 
-const mapLocation = (lat, lng) => {
-  var markerIcon = L.icon({
-    iconUrl: 'images/icon-location.svg',
+  const url =
+    searchType === 'IP'
+      ? `https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${inputValue}`
+      : `https://geo.ipify.org/api/v1?apiKey=${apiKey}&domain=${inputValue}`;
 
-    iconSize: [46, 56], // size of the icon
-    iconAnchor: [23, 55], // point of the icon which will correspond to marker's location
-  })
-  map.setView([lat, lng], 17)
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      myIpAddr.innerText = data.ip;
+      myLocation.innerText = `${data.location.region}, ${data.location.city}`;
+      myTimezone.innerText = `UTC ${data.location.timezone}`;
+      myIsp.innerText = data.isp;
+      mapDisplay(data.location.lat, data.location.lng);
+    })
+    .catch((error) => {
+      myIpAddr.innerText = '__';
+      myLocation.innerText = '__';
+      myTimezone.innerText = '__';
+      myIsp.innerText = '__';
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: false,
-  }).addTo(map)
+      const myInput = myForm.searchInput;
+      myInput.classList.add('error');
 
-  L.marker([lat, lng], { icon: markerIcon }).addTo(map)
-}
+      setTimeout(() => myInput.classList.remove('error'), 3000);
+      console.error(error);
+    });
+};
 
-//Search by IP + validation
-submitBtn.addEventListener('click', (event) => {
-  event.preventDefault()
-  if (
-    inputField.value.match(
-      /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/,
-    )
-  ) {
-    randomIP = inputField.value
-    url = `https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}=` + randomIP
-    fetch(url)
-      .then((response) => response.json())
-      .then((response) => {
-        ipAddress = response.ip
-        timeZone = response.time_zone.offset
-        countryLocation = response.country_name
-        cityLocation = response.city
-        postalCode = response.zipcode
-        isp = response.isp
-        lat = response.latitude
-        lng = response.longitude
+// Search for any IP addresses or domains and see the key information and location
+const regexIp = /^\b([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b(\.\b([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b){3}$/;
+const regexDomain = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/;
 
-        ipAddressField.innerHTML = ipAddress
-        timezoneInput.innerHTML = ` UTC ${timeZone}`
-        countryLocationInput.innerHTML = `${countryLocation}, ${cityLocation} ${postalCode}`
-        ispInput.innerHTML = isp
-        mapLocation(lat, lng)
-      })
-  } else {
-    alert('You have entered an invalid IP address!')
-    return false
+myForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const myInput = myForm.searchInput;
+
+  if (myInput.value.match(regexIp)) {
+    getData(myInput.value);
   }
-})
 
+  if (myInput.value.match(regexDomain)) {
+    getData(myInput.value, 'DOMAIN');
+  }
 
+  if (!myInput.value.match(regexDomain) && !myInput.value.match(regexIp)) {
+    myInput.classList.add('error');
 
-// // const myForm = document.querySelector('.submit-btn')
-// const submitBtn= document.querySelector('submit-btn')
-// const ipAddressField = document.querySelector('.ipAddressField')
-// const countryLocationInput = document.querySelector('.locationInput')
-// const timeZoneInput = document.querySelector('.timezoneInput')
-// const ispInput = document.querySelector('.ispInput')
+    setTimeout(() => myInput.classList.remove('error'), 3000);
+  }
 
-// // Map creation
-// const map = L.map('map').setView([51.505, -0.09], 13);
+  myInput.value = ''; 
+});
 
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//   attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-// }).addTo(map);
-
-// const myIcon = L.icon({
-//   iconUrl: './images/icon-location.svg',
-//   iconSize: [40, 50],
-//   iconAnchor: [20, 50]
-// });
-
-// let myMarker
-
-// const mapDisplay = (lat, lng) => {
-//   myMap.setView([lat, lng], 16)
-
-//   if (myMarker != null) myMarker.remove()
-
-//   myMarker = L.marker([lat, lng], { icon: myIcon }).addTo(map)
-
-//   myMarker.addTo(map)
-// }
-
-// // Get the info the page needs
-// const getData = (inputValue = '', searchType = 'ip') => {
-//   const apiKey = 'at_E6BD5wm4eWkq0e3q9TyyAmGNH165p';
-//   // const apiKey = 'YOUR_API_KEY'; // Replace with your actual API key
-//   const url =
-//     searchType === 'ip'
-//       ? `https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${inputValue}`
-//       : `https://geo.ipify.org/api/v1?apiKey=${apiKey}&domain=${inputValue}`;
-
-//   fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       ipAddressField.innerHTML = data.ip;
-//       countryLocationInput.innerHTML = `${data.location.region}, ${data.location.city}`;
-//       timeZoneInput.innerHTML = `UTC ${data.location.timezone}`;
-//       ispInput.innerHTML = data.isp;
-//       mapDisplay(data.location.lat, data.location.lng);
-//     })
-//     .catch((error) => {
-//       ipAddressField .innerHTML = '__';
-//       countryLocationInput.innerHTML = '__';
-//       timeZoneInput.innerHTML = '__';
-//       ispInput.innerHTML = '__';
-
-//       const myInput =  submitBtn.searchInput;
-//       myInput.classList.add('error');
-
-//       setTimeout(() => myInput.classList.remove('error'), 3000);
-//       console.error(error);
-//     });
-// };
-
-// // Search for any IP addresses or domains and see the key information and location
-// const regexIp = /^\b([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b(\.\b([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b){3}$/;
-// const regexDomain = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/;
-
-// submitBtn.addEventListener('submit', (e) => {
-//   e.preventDefault();
-
-//   const myInput =  submitBtn.searchInput;
-
-//   if (myInput.value.match(regexIp)) {
-//     getData(myInput.value)
-//   }
-
-//   if (myInput.value.match(regexDomain)) {
-//     getData(myInput.value, (searchType = 'DOMAIN'))
-//   }
-
-//   if (!myInput.value.match(regexDomain) && !myInput.value.match(regexIp)) {
-//     myInput.classList.add('error')
-
-//     setTimeout(() => myInput.classList.remove('error'), 3000)
-//   }
-// });
-
-// // Load IP Address on the map on the initial page load
-// getData();
+// Load IP Address on the map on the initial page load
+getData();
 
 
--- ipTracker/config.js
+++
@@ -1,5 +0,0 @@
-// config.js
-// const API_KEY = '9c095da267384cf0a9fccc8c7cb83ec5&ip'
-const API_KEY = '3440c4ff21044ccb90ce3bd800f8a847&ip'
-export default API_KEY
-
